c140cab5bc6bfa85a1ebb3d5df0b040bae558b6c
mathewgut <mathewgutoskie@gmail.com>
2024-06-21 19:41:51 -0400

    implemented removeFromArray function, tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
+    for(position in arguments){
+        if(position == 0){
+            continue
+        }else{
+            if(inputArray.indexOf(arguments[position]) == -1)
+                continue
+            else{
+                while (inputArray.indexOf(arguments[position]) != -1){
+                    inputArray.splice(inputArray.indexOf(arguments[position]), 1)
+                }
+            }
+        }
+    }
+    return inputArray
 };
 
+removeFromArray([3,2,1,1,7],3,2,1)
 // Do not edit below this line
 module.exports = removeFromArray;
