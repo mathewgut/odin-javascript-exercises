9abb133a4e0ac49498642a50d1cb38b798bb4cd1
mathewgut <mathewgutoskie@gmail.com>
2024-06-21 17:35:17 -0400

    finished string repeat function, passes tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat,repititions) {
+    stringToRepeat = String(stringToRepeat)
+    repititions = Number(repititions)
+    let repeatedString = ''
+    for(let i = 0; i < repititions; i++){
+        repeatedString += stringToRepeat;
+    }
+    return repeatedString;
 };
 
+repeatString("wow",5)
 // Do not edit below this line
 module.exports = repeatString;
