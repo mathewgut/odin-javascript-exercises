1be3dc44f6c33f6aad90608d94f0ee11ac0f8a3f
mathewgut <mathewgutoskie@gmail.com>
2024-06-21 17:59:29 -0400

    completed reverseString function, passes tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let reversedString = ''
+    stringSplit = inputString.split('')
+    console.log(stringSplit)
 
+    for(let i = stringSplit.length - 1; i > -1; i--){
+        reversedString += stringSplit[i];
+    }
+    return reversedString
 };
 
 // Do not edit below this line
