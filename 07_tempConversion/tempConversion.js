ec4df6530f91ba35033559426fbf8b43b922a356
mathewgut <mathewgutoskie@gmail.com>
2024-06-21 23:45:02 -0400

    functions convertToFahrenheit and convertToCelcius both created and pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  
+  let fConversion = (inputTemp -32) * 5/9;
+  fConversion = Math.round(fConversion *10)/10;
+  return fConversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  let cConversion = (inputTemp *9/5) + 32;
+  cConversion = Math.round(cConversion *10)/10;
+  return cConversion;
 };
 
 // Do not edit below this line
