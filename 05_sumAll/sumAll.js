0ca71b22d35b9fefc9c2303ae77cab41e2fc67e4
mathewgut <mathewgutoskie@gmail.com>
2024-06-21 21:25:11 -0400

    created sumAll function, passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    intOne = Number(intOne);
+    intTwo = Number(intTwo);
 
+    let sumTotal = 0;
+    let smallToBig = [];
+
+    intOne >= intTwo ? smallToBig.push(intOne) && 
+    smallToBig.push(intTwo) : false;
+
+    intTwo >= intOne ? smallToBig.push(intOne) && 
+    smallToBig.push(intTwo) : false;
+
+    for(let i = smallToBig[0]; i < (smallToBig[1] +1); i++){
+        sumTotal += i;
+    }
+    console.log(sumTotal)
+    console.log(smallToBig)
+    return sumTotal;
 };
+
+sumAll(3,5)
 
 // Do not edit below this line
 module.exports = sumAll;
