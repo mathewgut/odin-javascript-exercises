919d37574e46885f3f42c0c46aac611710977f6f
mathewgut <mathewgutoskie@gmail.com>
2024-06-21 21:46:19 -0400

    created leapYears function, passed tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    inputYear = Number(inputYear);
+    if(inputYear % 4 === 0){
+        if(inputYear % 100 === 0 && inputYear % 400 === 0){
+            return true;
+        }
+        else if(inputYear % 100 === 0 && inputYear % 400 !== 0){
+            return false;
+        }else {
+            return true;
+        }
 
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
