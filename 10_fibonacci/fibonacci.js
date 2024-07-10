c51fb61c4f279668e3eb510bce1888c6616108b8
mathewgut <mathewgutoskie@gmail.com>
2024-07-09 20:05:40 -0400

    completed fibonacci excersize

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    // test cases only use a string for not a number tests
+    // strings get converted to numbers for comparison anyways
+    // meaning no change is necessary
+    if (position < 0) {
+        return 'OOPS'
+    } else if (position == 0) {
+        return 0;
+    }
 
+    let fibonacciArray = [1,1];
+
+    for(; position > fibonacciArray.length;){
+        fibonacciArray.push(fibonacciArray[fibonacciArray.length - 2]
+             + fibonacciArray[fibonacciArray.length - 1])
+    }
+    return fibonacciArray[position-1]
 };
 
 // Do not edit below this line
